c121238d9e072a07db9ec950a34ec8741babfaf4
Orshovskyi Maksym <137319202+MaksymOrshovskyi@users.noreply.github.com>
2024-04-21 17:20:33 +0300

    task1

--- Lab9_10CharpT/Program.cs
+++ Lab9_10CharpT/Program.cs
@@ -1,8 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Lab#9  or  Lab#10");
-//  За бажанням студента для задач можна створювати консольний проект або WinForm
-// Бажано для задач лаб. робіт створити окремі класи
-// Виконання  виконати в стилі багатозаданості :
-//   Lab9T2  lab9task2 = new Lab9T2; lab9task2.Run();
-// При бажанні можна створити багатозадачний режим виконання задач.
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Шлях до файлу
+        string filePath = "text.txt";
+
+        // Читання тексту з файлу
+        string text = File.ReadAllText(filePath);
+
+        // Створення стеку для зберігання голосних букв
+        Stack<char> vowelsStack = new Stack<char>();
+
+        // Перебір кожного символу тексту
+        foreach (char c in text)
+        {
+            // Якщо символ є голосною буквою, додаємо його до стеку
+            if (IsVowel(c))
+            {
+                vowelsStack.Push(c);
+            }
+        }
+
+        // Виведення голосних букв у зворотньому порядку
+        while (vowelsStack.Count > 0)
+        {
+            Console.Write(vowelsStack.Pop());
+        }
+    }
+
+    // Метод для перевірки, чи є символ голосною буквою
+    static bool IsVowel(char c)
+    {
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+        return Array.IndexOf(vowels, c) != -1;
+    }
+}
 
